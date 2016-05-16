a6c53be47a0a7696e6de9527a5ef008b75781e5e
Mesut Can Gurle <mesutcang@gmail.com>
2016-05-17 00:29:31 +0300

    first plugin that just works.

--- wpcall/wpcall.php
+++ wpcall/wpcall.php
@@ -1,3 +1,17 @@
 <?php
-defined( 'ABSPATH' ) or die( 'Direct access to plugin is not allowed!' );
+/*
+ * Plugin Name: WPCall
+ * Version: 0.0.1
+ * Description: WPCall helps users to make calls over WebRTC.
+ * Author: Mesut Can Gurle
+ * Author URI: http://mesutcang.blogspot.com
+ * Plugin URI: https://github.com/mesutcang/wpcall
+ * Text Domain: wpcall
+ * Domain Path: /languages
+ * License: GPLv3
+ * License URI: http://www.gnu.org/licenses/gpl-3.0
+*/
+
+echo "WPCall"
+
 ?>
