c7b18b395a4d57a4e303838b318c8272abce410e
Mesut Can Gurle <mesutcang@gmail.com>
2016-05-17 23:53:31 +0300

    licence is added to plugin header.

--- wpcall/wpcall.php
+++ wpcall/wpcall.php
@@ -7,11 +7,21 @@
  * Author URI: http://mesutcang.blogspot.com
  * Plugin URI: https://github.com/mesutcang/wpcall
  * Text Domain: wpcall
- * Domain Path: /languages
  * License: GPLv3
  * License URI: http://www.gnu.org/licenses/gpl-3.0
-*/
+
+{Plugin Name} is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+any later version.
+ 
+{Plugin Name} is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+ 
+You should have received a copy of the GNU General Public License
+along with {Plugin Name}. If not, see {License URI}.*/
 
 echo "WPCall"
-
 ?>
